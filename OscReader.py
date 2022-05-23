76a0c073583d46784b06daa019d45a17725f67aa
cpaca <37684627+cpaca@users.noreply.github.com>
2022-05-23 13:29:31 -0700

    finished project
    got the data i need. keeping this on github incase i ever need to process more oscilloscope data in the future.

--- OscReader.py
+++ OscReader.py
@@ -1,5 +1,18 @@
+import os
+
+class OscilloscopeException(Exception):
+    pass
+
 class OscReader(object):
     def __init__(self, filename: str):
+        if not os.path.exists(filename):
+            if not filename.endswith(".csv"):
+                filename += ".csv"
+
+        if not os.path.exists(filename):
+            if not filename.startswith("osc-data"):
+                filename = "osc-data/" + filename
+
         with open(filename, "r") as f:
             data = f.readlines()
             for i in range(len(data)):
@@ -22,6 +35,7 @@ class OscReader(object):
                         # but whatever
                         row[j] = float(row[j])
                 data[i] = row
+        self.name = os.path.basename(filename)
         self.data = data
 
     # Checks whether this osc-reading and another osc reading
@@ -49,4 +63,79 @@ class OscReader(object):
                 return False
 
         return True
+
+    #   Output-filename: The filename of the output.
+    #       You can open it in OscReader later.
+    #   Probes: Which probes to copy and combine.
+    #       Sometimes you only want to combine one probe.
+    #   Oscs: The oscilloscopes you're going to combine.
+    @staticmethod
+    def combine(output_filename: str, probes: list, oscs: list):
+        for i in range(len(oscs)):
+            osc = oscs[i]
+            if type(osc) == OscReader:
+                pass # already an oscilloscope reading
+            elif type(osc) == str:
+                # transform it into an oscilloscope reading
+                oscs[i] = OscReader(oscs[i])
+            else:
+                raise OscilloscopeException("Cannot use " + str(osc) + " to create an oscilloscope reading.")
+
+        if len(oscs) < 2:
+            raise OscilloscopeException("Why would you need to combine less than two oscilloscope readings?")
+        first = oscs[0]
+        for osc in oscs:
+            if not osc.check(first):
+                raise OscilloscopeException("Oscilloscopes failed the check() function.")
+
+        max_probes = len(first.data[1])
+        for probe in probes:
+            if type(probe) != int:
+                raise OscilloscopeException("You need to number which probe you're using.")
+            if probe < 1:
+                raise OscilloscopeException("Probes are indiced starting at 1, not 0.")
+            if probe > max_probes:
+                raise OscilloscopeException("There are not " + probe + " probes.")
+
+        with open(output_filename, "w") as f:
+            # Because of the check() we know all of the oscilloscope readings have the same # of elements.
+            rows = len(first.data)
+
+            # Write the first row first.
+            # No comma at the end of the first column
+            # The later columns handle that
+            f.write("x-axis")
+            for osc in oscs:
+                for probe in probes:
+                    f.write(",")
+                    f.write(osc.name)
+                    f.write(" P" + str(probe))
+
+            f.write("\n")
+
+            # Second row.
+            # From the check() we know all of them have the same second row.
+            # So we can use any oscilloscope's second row, first column.
+            f.write(first.data[1][0])
+            # Next, go through the oscs and add their individual units.
+            # Shouldn't matter regardless (repeat "volts" a million times) but just for ssfety.
+            for osc in oscs:
+                for probe in probes:
+                    f.write(",")
+                    f.write(str(osc.data[1][probe]))
+
+            f.write("\n")
+            # Third row and beyond.
+            # We know they all have the same timestamp for each row.
+            # Just need to copy the data.
+            num_rows = len(first.data)
+            for row in range(num_rows):
+                if row < 2:
+                    continue
+                f.write(str(first.data[row][0]))
+                for osc in oscs:
+                    for probe in probes:
+                        f.write(",")
+                        f.write(str(osc.data[row][probe]))
+                f.write("\n")
 
--- main.py
+++ main.py
@@ -22,3 +22,9 @@ else:
     import sys
     sys.exit("Invalid oscilloscope readings")
 
+# Now to start combining oscilloscope data.
+OscReader.combine("output-oscs/1uFV123456.csv", [1], ["OCv1", "OCV2", "OCV3", "OCV4", "OCV5", "OCV6"])
+OscReader.combine("output-oscs/22uFV123456.csv", [2], ["OCV1", "OCV2", "OCV3", "OCV4", "OCV5", "OCV6"])
+OscReader.combine("output-oscs/1uFWithLoad.csv", [1], ["OCV6", "10MV6", "5MV6", "1MV6", "500kV6", "10kV6"])
+OscReader.combine("output-oscs/22uFWithLoad.csv", [2], ["OCV6", "10MV6", "5MV6", "1MV6", "500kV6", "10kV6"])
+
